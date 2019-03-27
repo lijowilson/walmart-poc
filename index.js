c25de16914247d9ded87592c7713f5aa6722d94e
Lijo k wilson <lijo.wilson@litmus7.com>
2019-03-27 16:08:42 +0530

    changes for including babel with babelrc file

--- index.js
+++
@@ -1,9 +0,0 @@
-//What is the best approach for babel. Is it to use .babelrc or use a seperate file
-
-require('babel-register')({
-    presets: [ 'env' ]
-})
-
-// Import the application
-require("@babel/polyfill")
-require('./server.js')
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+import "babel-polyfill"
 import express from "express"
 import router  from "./router/routes"
 let app = express()
