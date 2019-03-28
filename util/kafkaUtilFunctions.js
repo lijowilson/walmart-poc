8c270c94fb9fc9fba52edec9f14767eb5e098421
Lijo k wilson <lijo.wilson@litmus7.com>
2019-03-28 10:05:20 +0530

    changes for refactoring code to modularize and move to smaller functions

---
+++ util/kafkaUtilFunctions.js
@@ -0,0 +1,116 @@
+import * as puppeteerController from "../actions/puppeteerActions"
+import {emptyScrapingResponse} from "./utilFunctions"
+import * as kafka from "kafkajs"
+import propertiesReader from 'properties-reader'
+const properties = propertiesReader('./properties/config.properties')
+
+export const kafkaObject = () => {
+  
+  const KAFKA_BROKER = properties.get("kafka-host")
+  const CLIENT_ID = properties.get('kafka-CLIENT_ID')
+  
+  const kafkaObj = new kafka.Kafka({
+    brokers: [KAFKA_BROKER]
+    , clientId: CLIENT_ID
+  })
+  
+  return kafkaObj
+}
+
+export const kafkaConsumer = (kafka) => {
+  const CONSUMER_GROUP_NAME = properties.get('kafka-consumergroup')
+  const kafkaConsume = kafka().consumer({groupId: CONSUMER_GROUP_NAME})
+  
+  return kafkaConsume
+}
+
+export const signalTrapFunc = (signalTraps, obj, process, reject) => {
+  signalTraps.map(type => {
+    process.once(type, async () => {
+      try {
+        await obj.disconnect()
+      } finally {
+        process.kill(process.pid, type)
+        if (reject !== undefined)
+          reject("error")
+      }
+    })
+  })
+}
+
+export const errorTypesFunc = (errorTypes, obj, process, reject) => {
+  errorTypes.map(type => {
+    process.on(type, async () => {
+      try {
+        console.log(`process.on ${type}`)
+        await obj.disconnect()
+        process.exit(0)
+        if (reject !== undefined)
+          reject("error")
+      } catch (_) {
+        process.exit(1)
+      }
+    })
+  })
+}
+
+export const pushKafkaMsg = async (message, topicName, producerObj) => {
+  
+  const run = async () => {
+    try {
+      let stringMsg = JSON.stringify(message)
+      let tempMessage = [{value: stringMsg}]
+      await producerObj.connect()
+      await producerObj.send({
+        topic: topicName
+        , messages: tempMessage
+        //messages: JSON.stringify(message)
+      })
+      //return resolve("success")
+    } catch (er) {
+      console.log('error => ', er)
+      await producerObj.disconnect()
+      //return reject(er)
+      
+    }
+    await producerObj.disconnect()
+  }
+  return run();
+}
+
+export const readKafkaMsg = async (topicName, consumer) => {
+  const run = async () => {
+    await consumer.connect()
+    await consumer.subscribe({topic: topicName})
+    await consumer.run({
+      autoCommitInterval: 5000
+      , eachMessage: async ({topic, partition, message}) => {
+        if (message.value !== 'undefined') {
+          message = JSON.parse(message.value)
+          let [username, password] = [message.username, message.password]
+          const BASEURL = properties.get('walmart-baseurl')
+          const TGT_SELCTOR = properties.get('walmart-orderSection-selector')
+          
+          console.log('username' + username + 'password' + password)
+          let scrapingRepsonseTMP = emptyScrapingResponse()
+          
+          scrapingRepsonseTMP.scrapeJobId = message.scrapeJobId
+          scrapingRepsonseTMP.status = message.status
+          scrapingRepsonseTMP.orderIds = message.orderIds
+          try {
+            console.log('puppetter invoked...')
+            await puppeteerController.invokePuppeteer(BASEURL, username, password, TGT_SELCTOR, scrapingRepsonseTMP)
+            
+          } catch (err) {
+            console.log('puppetter error ==>', err.message)
+          }
+          console.log('puppetter complete...')
+          
+        }
+        
+        
+      }
+    })
+  }
+  return run()
+}
---
+++ util/utilFunctions.js
@@ -0,0 +1,79 @@
+import validator from "validator";
+import * as mongoController from "../model/mongoController";
+
+
+export const validateScrapeRequest = (...inputReqArray) => {
+  
+  let status = false;
+  let [username, password] = inputReqArray
+  
+  if (isNotEmptyAndUndefined(username)
+    && isNotEmptyAndUndefined(password)
+    && validator.isEmail(username)) {
+    status = true;
+  }
+  return status
+}
+
+export const performFetchInfo = (res, scrapeId) => {
+  
+  run()
+  
+  async function run() {
+    try {
+      let data = await mongoController.fetchInformation(scrapeId)
+      
+      if (data !== 'undefined' && data !== null) {
+        if (data.status === 'Invalid Parameter') {
+          res.status(400).send('Bad Request')
+        } else {
+          res.status(200).send(data)
+        }
+      } else {
+        res.status(500).send('API/Service not found')
+      }
+    } catch (err) {
+      if (err === 'Invalid Parameter') {
+        res.status(400).send('Bad Request')
+      } else {
+        console.log(err);
+        res.status(500).send('API/Service not found')
+      }
+    }
+  }
+}
+
+export const populateTransferObj = (...inputObj) => {
+  
+  let kafkaMessage = {}
+  let [username, password, scrapejobId, status, orderIds] = inputObj
+  
+  if (isNotEmptyAndUndefined(username))
+    kafkaMessage.username = username
+  if (isNotEmptyAndUndefined(password))
+    kafkaMessage.password = password
+  if (isNotEmptyAndUndefined(scrapejobId))
+    kafkaMessage.scrapeJobId = scrapejobId
+  if (isNotEmptyAndUndefined(status))
+    kafkaMessage.status = status
+  if (Array.isArray(orderIds))
+    kafkaMessage.orderIds = orderIds
+  
+  return kafkaMessage
+}
+
+export const isNotEmptyAndUndefined = (input) => {
+  let status = false
+  if (input !== 'undefined' && !validator.isEmpty(input)) status = true
+  
+  return status
+}
+
+export const emptyScrapingResponse = () => {
+  let scrapingRepsonseTMP = {
+    "scrapeJobId": ""
+    , "status": ""
+    , "orderIds": []
+  }
+  return scrapingRepsonseTMP
+}
