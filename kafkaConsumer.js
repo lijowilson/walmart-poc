aad7a6331d7329e34fe0802861e52a85094a6c42
Lijo k wilson <lijo.wilson@litmus7.com>
2019-03-28 09:58:53 +0530

    changes for refactoring code to modularize and move to smaller functions

--- kafkaConsumer.js
+++ kafkaConsumer.js
@@ -1,87 +1,20 @@
-import * as kafkaObj from 'kafkajs'
+import {
+  kafkaObject,
+  kafkaConsumer,
+  readKafkaMsg,
+  errorTypesFunc, signalTrapFunc
+} from "./util/kafkaUtilFunctions";
 import propertiesReader from 'properties-reader'
-import * as puppeteerController from './controller/puppeteerController'
+import process from 'process'
 let properties = propertiesReader('./properties/config.properties')
 
 //initialization of kafka-node object
-
-const KAFKA_BROKER = properties.get("kafka-host")
-const CLIENT_ID = properties.get('kafka-CLIENT_ID')
-const TOPIC = properties.get('kafka-topic')
-const CONSUMER_GROUP_NAME = properties.get('kafka-consumergroup')
-
-
-const kafka = new kafkaObj.Kafka({
-  brokers: [KAFKA_BROKER]
-  , clientId: CLIENT_ID
-})
-
-const consumer = kafka.consumer({groupId: CONSUMER_GROUP_NAME})
-
-const run = async () => {
-  await consumer.connect()
-  console.log('topicName' + TOPIC)
-  await consumer.subscribe({topic: TOPIC})
-  await consumer.run({
-    autoCommitInterval: 5000
-    , eachMessage: async ({topic, partition, message}) => {
-      let msgVal = message.value
-      console.log('message vlaue ' + msgVal)
-      if (typeof msgVal != "undefined") {
-        message = JSON.parse(msgVal)
-        let baseurl = properties.get('walmart-baseurl')
-        let username = message.username
-        let password = message.password
-        let targetSelector = properties.get('walmart-orderSection-selector')
-        let scrapingRepsonseTMP = {
-          "scrapeJobId": ""
-          , "status": ""
-          , "orderIds": []
-        }
-        scrapingRepsonseTMP.scrapeJobId = message.scrapeJobId
-        scrapingRepsonseTMP.status = message.status
-        scrapingRepsonseTMP.orderIds = message.orderIds
-        try{
-          console.log('puppetter invoked...')
-          await puppeteerController.invokePuppeteer(baseurl,username,password,targetSelector,scrapingRepsonseTMP)
-  
-        }catch(err){
-          console.log('puppetter error ==>',err.message)
-        }
-        console.log('puppetter complete...')
-       
-      }
-      
-      
-    }
-  })
-}
-
-run().catch(e => console.error(`[Kafka/consumer] ${e.message}`, e))
-
+const consumer = kafkaConsumer(kafkaObject)
+const KAFKA_TOPIC = properties.get('kafka-topic')
 const errorTypes = ['unhandledRejection', 'uncaughtException']
 const signalTraps = ['SIGTERM', 'SIGINT', 'SIGUSR2']
 
-errorTypes.map(type => {
-  process.on(type, async e => {
-    try {
-      console.log(`process.on ${type}`)
-      console.error(e)
-      await consumer.disconnect()
-      process.exit(0)
-    } catch (_) {
-      process.exit(1)
-    }
-  })
-})
-
-signalTraps.map(type => {
-  process.once(type, async () => {
-    try {
-      await consumer.disconnect()
-    } finally {
-      process.kill(process.pid, type)
-    }
-  })
-})
+readKafkaMsg(KAFKA_TOPIC,consumer)
+errorTypesFunc(errorTypes,consumer,process)
+signalTrapFunc(signalTraps,consumer,process)
 
