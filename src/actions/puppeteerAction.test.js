0a9bf6b2f14d24189b235fb4f920544e7c82429f
Lijo k wilson <lijo.wilson@litmus7.com>
2019-04-01 15:35:11 +0530

    puppeteer controller for invalid credential scenario

--- src/actions/puppeteerAction.test.js
+++ src/actions/puppeteerAction.test.js
@@ -1,4 +1,4 @@
-import {invokePuppeteer} from '/puppeteerActions';
+import {invokePuppeteer} from './puppeteerActions';
 import propertiesReader from 'properties-reader';
 import {
   createMongoConnection,
@@ -37,6 +37,40 @@ test('test for api with puppetter valid scenario'
       //things to validate are status should be complete, array size
       // should be bigger than 0
       expect(data.status).toEqual('complete');
+      
+    } catch (err) {
+      console.log(`error while invoking puppetteer ${err}`);
+    }
+  }, 50000);
+
+test('test for api with puppetter invalid credentials'
+  , async () => {
+    try {
+      //first insert an entry to db
+      let username = 'dima1@litmus7.com';
+      let password = 'Aa123456';
+      //creating customer object
+      let customObj = {
+        'scrapeJobId': ''
+        , 'status': 'in-progress'
+        , 'username': username
+        , 'password': password
+        , 'orderIds': []
+      };
+      createMongoConnection(mongoose);
+      let dbResponse1 = await saveToDB(customObj, customerObj);
+      //expect(1);
+      expect(dbResponse1.scrapeJobId.length).toBeGreaterThanOrEqual(1);
+      let scrapeJobId = dbResponse1.scrapeJobId;
+      customObj.scrapeJobId = scrapeJobId;
+      console.log(`scrapeJobId => ${scrapeJobId}`);
+      const baseURL = properties.get('walmart-baseurl');
+      const targetSelector = properties.get('walmart-orderSection-selector');
+      await invokePuppeteer(baseURL, username, password, targetSelector, customObj);
+      const data = await fetchwithMongoose(scrapeJobId, customerObj);
+      //things to validate are status should be complete, array size
+      // should be bigger than 0
+      expect(data.status).toEqual('invalid credentials');
       
     } catch (err) {
       console.log(`error while invoking puppetteer ${err}`);
